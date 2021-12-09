189c70ede245cd9908c76614580ea8c65c8d8b48
simran7s <simran7s@my.yorku.ca>
2021-12-09 16:10:19 -0500

    reading terminal inputs from user

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace CSharpTutorial
              Multiline comment
              */
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
